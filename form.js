822ae84c4799d7068bd6d91cf9dceaf576457b57
Shubham Nishad <shubhamnishad25@gmail.com>
2017-03-12 19:34:32 +0530

    made a lot of changes to form.html
    made javascript internal and added a bit of styling to the form

--- form.js
+++ form.js
@@ -1,16 +1,2 @@
-function check(){
-var fullname=document.getElementById("fullname").value;
-var email=document.getElementById("email").value;
-var password=document.getElementById("password").value;
-var confirmpassword=document.getElementById("confirmpassword").value;
-var error=document.getElementById("errorField");
 
-if(password!=confirmpassword){
-	error.innerHTML="confirm password correctly";
-	return false;
-}
-console.log(fullname);
-console.log(email);
-console.log(password);
-console.log(confirmpassword);
-}
+
